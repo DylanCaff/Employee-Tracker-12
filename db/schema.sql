00635d13fb38cfdf9c06c48677e3eb00d30ba3c2
DylanCaff <dylancaff0902@gmail.com>
2023-03-22 01:20:35 -0400

    began table role

--- db/schema.sql
+++ db/schema.sql
@@ -2,6 +2,13 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 CREATE TABLE department (
-    id: INT PRIMARY KEY AUTO_INCREMENT,
-    name: VARCHAR(30) NOT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_ id INTEGER
+)
