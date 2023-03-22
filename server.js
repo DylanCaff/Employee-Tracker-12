c643c5287c4206a89dc195b5efab165fa47c7297
DylanCaff <dylancaff0902@gmail.com>
2023-03-22 01:15:07 -0400

    added create table for department

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const cTable = require('console.table');
 require('dotenv').config();
@@ -80,5 +80,16 @@ function promptUser() {
             if (choices === "No Action") {
                 connection.end()
             };
-        })
-}
+        });
+};
+
+showDepartments = () => {
+    console.log('Showing all departments...\n');
+    const sql = `SELECT department.id AS id, department.name AS department FROM department`; 
+  
+    connection.promise().query(sql, (err, rows) => {
+      if (err) throw err;
+      console.table(rows);
+      promptUser();
+    });
+  };
