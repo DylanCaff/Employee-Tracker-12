bb3a86ad2326b92011e661484a1098a28f24e940
DylanCaff <dylancaff0902@gmail.com>
2023-03-27 13:59:10 -0400

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,7 +7,7 @@ VALUES (1, "Engineering"),
 INSERT INTO role (department_id, title, salary)
 VALUES (1, "Sales Lead", 100000),
        (2, "Salesperson", 80000),
-       (3, "Lead Engineer", 150000)
+       (3, "Lead Engineer", 150000),
        (4, "Software Engineer", 120000),
        (5, "Account Manager", 160000),
        (6, "Accountant", 125000),
