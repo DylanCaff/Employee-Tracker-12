2b7e6233a0924025a9f5719e3ae72de391c8796c
DylanCaff <dylancaff0902@gmail.com>
2023-03-29 19:36:04 -0400

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,28 +1,28 @@
 INSERT INTO department (id, name)
-VALUES (1, "Engineering2"),
-       (2, "Finance2"),
-       (3, "Legal2"),
-       (4, "Sales2");
+VALUES (1, 'Engineering2'),
+       (2, 'Finance2'),
+       (3, 'Legal2'),
+       (4, 'Sales2');
 
 INSERT INTO role (department_id, title, salary)
-VALUES (1, "Sales Lead", 100000),
-       (2, "Salesperson", 80000),
-       (3, "Lead Engineer", 150000),
-       (4, "Software Engineer", 120000),
-       (1, "Account Manager", 160000),
-       (2, "Accountant", 125000),
-       (3, "Legal Team Lead", 250000),
-       (4, "Lawyer", 190000);
+VALUES (1, 'Sales Lead', 100000),
+       (2, 'Salesperson', 80000),
+       (3, 'Lead Engineer', 150000),
+       (4, 'Software Engineer', 120000),
+       (1, 'Account Manager', 160000),
+       (2, 'Accountant', 125000),
+       (3, 'Legal Team Lead', 250000),
+       (4, 'Lawyer', 190000);
 
 INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("John", "Doe", 1),
-       ("Mike", "Chan", 1);
+VALUES ('John', 'Doe', 1),
+       ('Mike', 'Chan', 2);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Mike", "Chan", 2, 1),
-       ("Ashley", "Rogriguez", 3, 1),
-       ("Kevin", "Tupik", 4, 1),
-       ("Kunal", "Singh", 5, 2),
-       ("Malia", "Brown", 6, 2),
-       ("Sarah", "Lourd", 7, 2),
-       ("Tom", "Allen", 8, 2);
+VALUES ('Mike', 'Chan', 2, 1),
+       ('Ashley', 'Rodriguez', 3, 1),
+       ('Kevin', 'Tupik', 4, 1),
+       ('Kunal', 'Singh', 5, 2),
+       ('Malia', 'Brown', 6, 2),
+       ('Sarah', 'Lourd', 7, 2),
+       ('Tom', 'Allen', 8, 3);
