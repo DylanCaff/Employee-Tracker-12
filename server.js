fdf74f1514ff558dfc50f1220d129f928f198aa4
DylanCaff <dylancaff0902@gmail.com>
2023-03-29 18:37:33 -0400

    wanted to see if that fixerd issue

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ function promptUser() {
             const { choice } = answers;
 
             if (choice === "View All Employees") {
-                showEmployees();
+                showDepartments();
             }
 
             if (choice === "Add Employee") {
@@ -108,19 +108,9 @@ function promptUser() {
         });
 };
 
-showEmployees = () => {
-    console.log('Showing all employees...\n'); 
-    const sql = `SELECT employee.id, 
-    employee.first_name, 
-    employee.last_name, 
-    role.title, 
-    department.name AS department,
-    role.salary, 
-    CONCAT (manager.first_name, " ", manager.last_name) AS manager
-FROM employee
-    LEFT JOIN role ON employee.role_id = role.id
-    LEFT JOIN department ON role.department_id = department.id
-    LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+showDepartments = () => {
+    console.log('Showing all departments...\n');
+    const sql = `SELECT department.id AS id, department.name AS name FROM department`;
 
     connection.query(sql, (err, rows) => {
         if (err) throw err;
